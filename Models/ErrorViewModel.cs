ae20398d91a3bf0fcb728990aa9b8d57ab671e4b
Tu-Chu <tuchuthien2710@gmail.com>
2022-08-25 23:25:37 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ChuThienTu639.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
